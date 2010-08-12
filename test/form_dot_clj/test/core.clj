2b579074c7ac0c2ce4c832922d871ba994088745
alienscience <saul@alienscience.org.uk>
2010-08-12 20:35:34 +0100

    Added type converstion tests

--- test/form_dot_clj/test/core.clj
+++ test/form_dot_clj/test/core.clj
@@ -2,7 +2,8 @@
   "Tests for form-dot-clj as a whole"
   (:use clojure.test)
   (:use form-dot-clj.core)
-  (:use form-dot-clj.html-controls))
+  (:use form-dot-clj.html-controls)
+  (:use clj-time.core))
 
 (def-field string-field
   [:maxlength 10]
@@ -27,6 +28,29 @@
   :url-field (textbox url-field)
   :bool-field (textbox bool-field))
 
+(defn has-value
+  "Checks that the given param map converts to the given value
+   map"
+  [params expected-values]
+  (let [[validated errors] (validate test-form params)]
+    (is (= validated expected-values) "Expected values")))
+  
+(deftest conversion
+  (has-value {"string-field" "moo"}
+             {:string-field "moo"})
+  (has-value {"int-field" "2"}
+             {:int-field 2})
+  (has-value {"float-field" "-0.8"}
+             {:float-field (Float. -0.8)})
+  (has-value {"date-field" "2010-08-18"}
+             {:date-field (date-time 2010 8 18)})
+  (has-value {"bool-field" "yes"} {:bool-field true})
+  (has-value {"bool-field" "true"} {:bool-field true})
+  (has-value {"bool-field" "NO"} {:bool-field false})
+  (has-value {"bool-field" "no"} {:bool-field false})
+  (has-value {"bool-field" "false"} {:bool-field false})
+  (has-value {"bool-field" "FALSE"} {:bool-field false}))
+  
 (defn validate-ok
   "Checks that the given parameters validate with no errors"
   [params]
@@ -38,7 +62,7 @@
   [params expected-errs]
   (let [[validated errors] (validate test-form params)]
     (is (= errors expected-errs) "Expected errors")))
-
+  
 (deftest validation
   (validate-ok {"string-field" "moo"})
   (validate-ok {"int-field" "10"})
